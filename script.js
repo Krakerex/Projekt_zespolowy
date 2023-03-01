c5f2adad8b59fb8702ba5cd06a4142b7d041e6d6
SzymonFigger <19720@student.ans-elblag.pl>
2023-03-01 18:47:24 +0100

    a

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ x0=Math.round(x);
 deltax=x0-x;
 fx0=root()
 }
+
+function przyklad1(){
+
+    const n = 4;
+    const x = 80.7;
+    const x0 = 0;
+    const deltax = 0;
+    const fun = 0;
+    const poch = 0;
+
+    x0 = Math.round(x);
+    deltax = x0 - x;
+
+    const roz = document.getElementById('rozw').innerHTML = deltax;
+}

29397af962215ffc8113cd34e47de77bd17c25d6
Krakerex <58233618+Krakerex@users.noreply.github.com>
2023-03-01 16:24:54 +0100

    s

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-function root(){
-    x=document.getElementById('podstawa').value;
-    y=document.getElementById('stopien').value;
-    document.getElementById('wynik').value = Math.pow(x,1/y);
+function root(x,y){
+
+    return Math.pow(x,1/y);
 }

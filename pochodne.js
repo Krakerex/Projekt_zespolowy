03cb00fe1c2cc6f69a5b8facea0dca1090e9298b
SzymonFigger <19720@student.ans-elblag.pl>
2023-04-19 19:56:19 +0200

    obliczanie pochodnej z definicji

--- pochodne.js
+++ pochodne.js
@@ -27,3 +27,29 @@ function formP(){
     const poch = math.derivative(wzor, 'x');
     druk(poch);
 }
+function test(){
+    const test = `
+        <h4>Wybrany wzór: $\\dfrac{1}{x^n}$ </h4>
+        <label for="n">
+            Podaj n 
+        </label>
+        <input type="number" id="n" name = "n" value = "1">
+        <br><br>
+        <button onclick=test1()>
+            Pochodna
+        </button>
+    `;
+    document.getElementById("testoweformy").innerHTML = test;
+    MathJax.typeset();
+}
+function test1(){
+    //var n = document.getElementById("n").value;
+    const t = "$$f'(x)=\\lim_{\\Delta x\\rightarrow 0}\\frac{f(x + \\Delta) - f(x)}{\\Delta x}$$ <br>" 
+               + "$$ f'(x_0)=\\lim_{\\Delta x \\rightarrow 0} \\frac{\\frac{1}{x_0 + \\Delta x} - \\frac{1}{x_0}}{\\Delta x} = "
+               + " \\lim_{\\Delta x \\rightarrow 0} \\frac{\\frac{x_0 - x_0 - \\Delta x}{(x_0 + \\Delta x)x_0}}{\\Delta x} = "
+               + " \\lim_{\\Delta x \\rightarrow 0} \\frac{- \\Delta x}{(x_0 + \\Delta x)x_0 \\cdot \\Delta x} = "
+               + " \\lim_{\\Delta x \\rightarrow 0} \\frac{-1}{(x_0 + \\Delta x)x_0} = "
+               + " - \\frac{1}{x_0 ^ 2} $$";
+    document.getElementById("rozw").innerHTML = t;
+    MathJax.typeset();
+}
